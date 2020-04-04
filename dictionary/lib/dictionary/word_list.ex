8ba32b1330b36ca418e80f7e414b1b2a759af296
Luján Fernaud <lujanfernaud@gmail.com>
2020-04-04 19:43:00 +0100

    Convert Dictionary into an application

--- dictionary/lib/dictionary/word_list.ex
+++ dictionary/lib/dictionary/word_list.ex
@@ -1,4 +1,10 @@
 defmodule Dictionary.WordList do
+  @process_name __MODULE__
+
+  def start_link do
+    Agent.start_link(&word_list/0, name: @process_name)
+  end
+
   def word_list do
     "../../assets/words.txt"
     |> Path.expand(__DIR__)
@@ -6,7 +12,7 @@ defmodule Dictionary.WordList do
     |> String.split()
   end
 
-  def random_word(word_list) do
-    Enum.random(word_list)
+  def random_word() do
+    Agent.get(@process_name, &Enum.random/1)
   end
 end
